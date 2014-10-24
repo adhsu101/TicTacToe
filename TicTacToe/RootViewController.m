647ecbc9033ec41ecce7e12eeeda6a78a758180f
Valak12 <Kohnechi@gmail.com>
2014-10-23 17:44:40 -0700

    Second commit

--- TicTacToe/RootViewController.m
+++ TicTacToe/RootViewController.m
@@ -9,6 +9,16 @@
 #import "RootViewController.h"
 
 @interface RootViewController ()
+@property (strong, nonatomic) IBOutlet UILabel *label1;
+@property (strong, nonatomic) IBOutlet UILabel *label2;
+@property (strong, nonatomic) IBOutlet UILabel *label3;
+@property (strong, nonatomic) IBOutlet UILabel *label4;
+@property (strong, nonatomic) IBOutlet UILabel *label5;
+@property (strong, nonatomic) IBOutlet UILabel *label6;
+@property (strong, nonatomic) IBOutlet UILabel *label7;
+@property (strong, nonatomic) IBOutlet UILabel *label8;
+@property (strong, nonatomic) IBOutlet UILabel *label9;
+@property (strong, nonatomic) IBOutlet UILabel *whichPlayerLabel;
 
 @end
 
@@ -16,12 +26,34 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.whichPlayerLabel.text = @"Player X";
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(UILabel *)findLabelUsingPoint: (CGPoint)point
+{
+    if (CGRectContainsPoint(self.label1.frame, point)) {
+        //Set X or O depending on player
+        return self.label1;
+    
+    }else
+    {
+        return self.label2;
+    }
 }
+- (IBAction)onLabelTapped:(UITapGestureRecognizer *)gesture
+{
+    CGPoint point = [gesture locationInView:self.view];
+    UILabel *selectedSquare =[self findLabelUsingPoint:point];
+    selectedSquare.text = @"X";
+    if ([self.whichPlayerLabel.text isEqualToString:@"Player X"]) {
+        self.whichPlayerLabel.text = @"Player Y";
+    }
+    else
+    {
+        self.whichPlayerLabel.text = @"Player X";
+    }
+}
+
+
 
 @end
